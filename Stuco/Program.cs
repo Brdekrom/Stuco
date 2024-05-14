9bcaed2ff4aec3c62138dc4abf79397a28dd4b24
Bryan <dekrobr@cronos.be>
2024-05-14 21:50:07 +0200

    Move to Extension class and register Stukadoor Endpoints extension into program

--- Stuco/Program.cs
+++ Stuco/Program.cs
@@ -1,7 +1,7 @@
+using Stuco.Api.ApiExtensions;
 using Stuco.Application;
 using Stuco.Application.Features.GetKlanten.Abstraction;
 using Stuco.Application.Features.Projecten.Abstractions;
-using Stuco.Application.Features.Stukadoren.Abstractions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -64,14 +64,6 @@ app.MapGet("/project/{id:int}", (int id, IGetProject getProject) =>
     return getProject.Execute(id);
 });
 
-app.MapGet("/stukadoor", (IGetAllStukadoren getAllStucadoren) =>
-{
-    return getAllStucadoren.Execute();
-});
-
-app.MapGet("/stukadoor/{id:int}", (int id, IGetStukadoor getStukadoor) =>
-{
-    return getStukadoor.Execute(id);
-});
+app.MapStukadoorEndpoints();
 
 app.Run();
