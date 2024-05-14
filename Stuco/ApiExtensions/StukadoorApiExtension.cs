5143e0d2fe7bc29c5cb5693f1f733b5ae9ff5af9
Bryan <dekrobr@cronos.be>
2024-05-14 21:47:38 +0200

    Create minimal API's extensions folder using Stukadoor as first feature

---
+++ Stuco/ApiExtensions/StukadoorApiExtension.cs
@@ -0,0 +1,27 @@
+﻿using Stuco.Application.Features.Stukadoren.Abstractions;
+using Stuco.Domain.Entities;
+
+namespace Stuco.Api.ApiExtensions;
+
+internal static class StukadoorApiExtension
+{
+    private const string BaseUrl = "/stukadoor";
+
+    internal static void MapStukadoorEndpoints(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapGet(BaseUrl, (IGetAllStukadoren getAllStucadoren) =>
+        {
+            return getAllStucadoren.Execute();
+        });
+
+        endpoints.MapGet("{BaseUrl}/{id:int}", (int id, IGetStukadoor getStukadoor) =>
+        {
+            return getStukadoor.Execute(id);
+        });
+
+        endpoints.MapPost(BaseUrl, (Stukadoor stukadoor, ICreateStukadoor createStukadoor) =>
+        {
+            return createStukadoor.Execute(stukadoor);
+        });
+    }
+}
