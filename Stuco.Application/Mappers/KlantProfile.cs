b0e1b579bc7338d4d5969854b522d0cef0089186
Bryan <dekrobr@cronos.be>
2024-06-05 00:00:32 +0200

    Create profile for automapper

---
+++ Stuco.Application/Mappers/KlantProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Stuco.Application.Features.Dtos;
+using Stuco.Domain.Entities;
+
+namespace Stuco.Application.Mappers;
+
+public class KlantProfile : Profile
+{
+    public KlantProfile()
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            CreateMap<KlantDto, Klant>();
+        });
+
+    }
+}
