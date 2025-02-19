f1420de01d57229809a98f5bb0e5aabf8b45c409
Bryan <dekrobr@cronos.be>
2025-02-19 11:09:33 +0100

    Add project files

---
+++ Stuco.Infrastructure.DocumentGenerator.Test/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Stuco.Infrastructure.DocumentGenerator.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ Stuco.Infrastructure.Xceed/DependencyInjection.cs
@@ -0,0 +1,9 @@
+﻿namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddXceedInfrastructure(this IServiceCollection services)
+    {
+        return services;
+    }
+}
